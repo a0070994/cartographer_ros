d19148b0f75f27c5be15ae87c0d79aac85727a38
Kevin Vorwerk <kevin.vorwerk@kiongroup.com>
2025-07-24 11:31:23 +0200

    Modified cartographer lua configuration to handle ngp simulation

--- cartographer_ros/configuration_files/backpack_2d_ngp.lua
+++ cartographer_ros/configuration_files/backpack_2d_ngp.lua
@@ -19,7 +19,7 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link",
+  tracking_frame = "navigation_lidar_link",
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = true,
@@ -28,9 +28,9 @@ options = {
   use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 0,
-  num_multi_echo_laser_scans = 1,
-  num_subdivisions_per_laser_scan = 10,
+  num_laser_scans = 1,
+  num_multi_echo_laser_scans = 0,
+  num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
@@ -44,6 +44,19 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
+TRAJECTORY_BUILDER_2D.max_range = 30.0
+TRAJECTORY_BUILDER_2D.min_range = 0.05
+TRAJECTORY_BUILDER_2D.min_z = 0.0
+TRAJECTORY_BUILDER_2D.max_z = 3.0
+
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10.0
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40.0
+
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
+
+
 
 return options
