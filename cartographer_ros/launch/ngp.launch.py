10386a518743a8f647c6a0b863845e494c6aae74
Kevin Vorwerk <kevin.vorwerk@kiongroup.com>
2025-08-04 11:49:06 +0200

    Updated npg.launch.py to handle map loading

--- cartographer_ros/launch/ngp.launch.py
+++ cartographer_ros/launch/ngp.launch.py
@@ -1,60 +1,56 @@
-"""
-  Copyright 2018 The Cartographer Authors
-  Copyright 2022 Wyca Robotics (for the ros2 conversion)
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-"""
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, SetRemap
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
 
 def generate_launch_description():
-
     ## ***** Launch arguments *****
-    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value = 'True')
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='True')
+    load_state_filename_arg = DeclareLaunchArgument('load_state_filename', default_value='')
+    config_basename_arg = DeclareLaunchArgument('configuration_basename', default_value='backpack_2d_ngp.lua')
+    config_directory_arg = DeclareLaunchArgument(
+        'configuration_directory',
+        default_value=FindPackageShare('cartographer_ros')
+                     .find('cartographer_ros') + '/configuration_files'
+    )
 
-    ## ***** File paths ******
-    pkg_share = FindPackageShare('cartographer_ros').find('cartographer_ros')
+    ## ***** Launch configurations *****
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    config_basename = LaunchConfiguration('configuration_basename')
+    config_directory = LaunchConfiguration('configuration_directory')
+    load_state_filename = LaunchConfiguration('load_state_filename')
 
     ## ***** Nodes *****
     cartographer_node = Node(
-        package = 'cartographer_ros',
-        executable = 'cartographer_node',
-        parameters = [{'use_sim_time': LaunchConfiguration('use_sim_time')}],
-        arguments = [
-            '-configuration_directory', FindPackageShare('cartographer_ros').find('cartographer_ros') + '/configuration_files',
-            '-configuration_basename', 'backpack_2d_ngp.lua'],
-        remappings = [('scan', 'navigation_lidar_scan')],
-        output = 'screen'
-        )
+        package='cartographer_ros',
+        executable='cartographer_node',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[
+            '-configuration_directory', config_directory,
+            '-configuration_basename', config_basename,
+            '--load_state_filename', load_state_filename
+        ],
+        remappings=[('scan', 'navigation_lidar_scan')],
+        output='screen'
+    )
 
     cartographer_occupancy_grid_node = Node(
-        package = 'cartographer_ros',
-        executable = 'cartographer_occupancy_grid_node',
-        parameters = [
-            {'use_sim_time': True},
-            {'resolution': 0.05}],
-        )
+        package='cartographer_ros',
+        executable='cartographer_occupancy_grid_node',
+        parameters=[
+            {'use_sim_time': use_sim_time},
+            {'resolution': 0.05}
+        ],
+        output='screen'
+    )
 
     return LaunchDescription([
         use_sim_time_arg,
-        # Nodes
+        load_state_filename_arg,
+        config_basename_arg,
+        config_directory_arg,
         cartographer_node,
-        cartographer_occupancy_grid_node,
+        cartographer_occupancy_grid_node
     ])
